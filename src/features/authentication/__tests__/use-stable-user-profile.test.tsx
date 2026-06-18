1bf8d9cbebc6aabf44f86efbac61f2b57f97dcbe
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-06-18 13:44:59 +0200

    reduce duplication
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/features/authentication/__tests__/use-stable-user-profile.test.tsx
+++ src/features/authentication/__tests__/use-stable-user-profile.test.tsx
@@ -13,6 +13,15 @@ import { useStableUserProfile } from '../hooks/use-stable-user-profile';
 // test function used only to count the number of render from TestComponent
 const renderSpy = vi.fn();
 
+const initialProfile = {
+    sub: '123',
+    name: 'userName',
+    email: 'user@test.com',
+    profile: 'user',
+    exp: 123,
+    access_token: 'old-token',
+};
+
 function TestComponent() {
     const userProfile = useStableUserProfile();
 
@@ -21,88 +30,55 @@ function TestComponent() {
     return null;
 }
 
+function renderTestComponent() {
+    const { wrapper, store } = createTestContext();
+
+    const updateProfile = (changes: Partial<typeof initialProfile> = {}) => {
+        // TODO: replace with setLoggedUser from commons-ui when it no longer has to be mocked
+        act(() => {
+            store.dispatch({
+                type: USER,
+                user: {
+                    profile: {
+                        ...initialProfile,
+                        ...changes,
+                    },
+                },
+            });
+        });
+    };
+
+    updateProfile();
+    render(<TestComponent />, { wrapper });
+
+    return { updateProfile };
+}
+
 describe('useStableUserProfile', () => {
     beforeEach(() => {
         renderSpy.mockClear();
     });
 
     it('should not rerender when only ignored user profile fields change', () => {
-        const { wrapper, store } = createTestContext();
-
-        const initialProfile = {
-            sub: '123',
-            name: 'userName',
-            email: 'user@test.com',
-            profile: 'user',
-            exp: 123,
-            access_token: 'old-token',
-        };
-
-        // TODO: change all store disptach user with setLoggedUser from commons-ui when it does not have to be mocked
-        act(() => {
-            store.dispatch({
-                type: USER,
-                user: {
-                    profile: initialProfile,
-                },
-            });
-        });
-
-        render(<TestComponent />, { wrapper });
+        const { updateProfile } = renderTestComponent();
 
         expect(renderSpy).toHaveBeenCalledTimes(1);
 
-        act(() => {
-            store.dispatch({
-                type: USER,
-                user: {
-                    profile: {
-                        ...initialProfile,
-                        exp: 456,
-                        access_token: 'new-token',
-                    },
-                },
-            });
+        updateProfile({
+            exp: 456,
+            access_token: 'new-token',
         });
 
         expect(renderSpy).toHaveBeenCalledTimes(1);
     });
 
     it('should rerender when an identity user profile field changes', () => {
-        const { wrapper, store } = createTestContext();
-
-        const initialProfile = {
-            sub: '123',
-            name: 'userName',
-            email: 'user@test.com',
-            profile: 'user',
-            exp: 123,
-            access_token: 'old-token',
-        };
-
-        act(() => {
-            store.dispatch({
-                type: USER,
-                user: {
-                    profile: initialProfile,
-                },
-            });
-        });
-
-        render(<TestComponent />, { wrapper });
+        const { updateProfile } = renderTestComponent();
 
         expect(renderSpy).toHaveBeenCalledTimes(1);
 
-        act(() => {
-            store.dispatch({
-                type: USER,
-                user: {
-                    profile: {
-                        ...initialProfile,
-                        email: 'new@test.com',
-                    },
-                },
-            });
+        updateProfile({
+            email: 'new@test.com',
         });
 
         expect(renderSpy).toHaveBeenCalledTimes(2);
