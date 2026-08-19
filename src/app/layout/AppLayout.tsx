1ed360eb814444ab2eeb18723277b27c0732e7b5
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-19 12:00:12 +0200

    add developper mode above sidebar
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -1,5 +1,8 @@
 import { Box, Stack } from '@mui/material';
 import { PropsWithChildren } from 'react';
+import { DevModeBanner, PARAM_DEVELOPER_MODE } from '@gridsuite/commons-ui';
+import { useAppParameterState } from 'features/app-parameters/hooks/use-app-parameter-state';
+import { useStableUserProfile } from 'features/authentication/hooks/use-stable-user-profile';
 import { AppSideBar } from '../../features/side-bar/components/AppSideBar';
 
 export type AppTopBarProps = {
@@ -7,10 +10,16 @@ export type AppTopBarProps = {
 };
 
 export function AppLayout({ onLogoutClick, children }: Readonly<PropsWithChildren<AppTopBarProps>>) {
+    const [isDeveloperMode] = useAppParameterState(PARAM_DEVELOPER_MODE);
+    const userProfile = useStableUserProfile() ?? undefined;
+
     return (
-        <Stack direction="row" height="100vh" width="100%" overflow="hidden">
-            <AppSideBar onLogoutClick={onLogoutClick} />
-            <Box sx={{ flex: 1 }}>{children}</Box>
-        </Stack>
+        <>
+            {userProfile && isDeveloperMode && <DevModeBanner />}
+            <Stack direction="row" height="100vh" width="100%" overflow="hidden">
+                <AppSideBar onLogoutClick={onLogoutClick} />
+                <Box sx={{ flex: 1 }}>{children}</Box>
+            </Stack>
+        </>
     );
 }
