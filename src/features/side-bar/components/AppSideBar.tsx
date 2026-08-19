1f32de244e13dc22dfbefb3818e01f4d3a90310d
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-19 10:28:30 +0200

    use sidebar from commons-ui
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/features/side-bar/components/AppSideBar.tsx
+++ src/features/side-bar/components/AppSideBar.tsx
@@ -1,42 +1,43 @@
-import { Box, Stack } from '@mui/material';
-import { useState } from 'react';
-import { AppSidebarHeader } from './AppSidebarHeader';
-import { AppSidebarFooter } from './footer/AppSidebarFooter';
+import {
+    AppSideBar as CommonAppSideBar,
+    PARAM_DEVELOPER_MODE,
+    PARAM_LANGUAGE,
+    PARAM_THEME,
+} from '@gridsuite/commons-ui';
 import { InvertedThemeProvider } from './InvertedThemeProvider';
+import { useAppParameterState } from '../../app-parameters/hooks/use-app-parameter-state';
+import { APP_NAME } from '../../../app/config/app-config';
+import { useStableUserProfile } from '../../authentication/hooks/use-stable-user-profile';
+import { fetchVersion } from '../../../shared/config/version';
+import { getServersInfos } from '../../top-bar/api/get-servers-infos';
+import AppPackage from '../../../../package.json';
 
 type SidebarProps = {
     onLogoutClick?: () => void;
 };
 
 export function AppSideBar({ onLogoutClick }: Readonly<SidebarProps>) {
-    const [isMinimized, setIsMinimized] = useState(true);
-
-    const toggleSideBarMinimized = (): void => {
-        setIsMinimized((previousIsSideBarMinimized) => !previousIsSideBarMinimized);
-    };
+    const [theme, setTheme] = useAppParameterState(PARAM_THEME);
+    const [selectedLanguage, setSelectedLanguage] = useAppParameterState(PARAM_LANGUAGE);
+    const [isDeveloperMode, handleChangeDeveloperMode] = useAppParameterState(PARAM_DEVELOPER_MODE);
+    const userProfile = useStableUserProfile() ?? undefined;
     return (
         <InvertedThemeProvider>
-            <Stack
-                component="aside"
-                sx={{
-                    width: isMinimized ? 64 : 224,
-                    height: '100%',
-                }}
-            >
-                <AppSidebarHeader isMinimized={isMinimized} />
-
-                <Box
-                    sx={{
-                        flex: 1,
-                    }}
-                />
-
-                <AppSidebarFooter
-                    isMinimized={isMinimized}
-                    toggleSideBarMinimized={toggleSideBarMinimized}
-                    onLogoutClick={onLogoutClick}
-                />
-            </Stack>
+            <CommonAppSideBar
+                isDeveloperMode={isDeveloperMode}
+                handleChangeDeveloperMode={handleChangeDeveloperMode}
+                currentTheme={theme}
+                setTheme={setTheme}
+                selectedLanguage={selectedLanguage}
+                setSelectedLanguage={setSelectedLanguage}
+                appName={APP_NAME}
+                userProfile={userProfile}
+                globalVersionPromise={() => fetchVersion().then((res) => res?.deployVersion ?? 'unknown')}
+                additionalModulesPromise={getServersInfos}
+                onLogoutClick={onLogoutClick}
+                appVersion={AppPackage.version}
+                appLicense={AppPackage.license}
+            />
         </InvertedThemeProvider>
     );
 }
