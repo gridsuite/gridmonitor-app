bf71a128da234110fb82ebb54ea905c5efd5642a
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-08-20 11:15:06 +0200

    add copyright headers
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/features/side-bar/components/InvertedThemeProvider.tsx
+++ src/features/side-bar/components/InvertedThemeProvider.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import { ThemeProvider, ScopedCssBaseline, createTheme } from '@mui/material';
 import { DARK_THEME, LIGHT_THEME, PARAM_THEME } from '@gridsuite/commons-ui';
 import { getAppTheme } from '../../../app/config/app-theme';
