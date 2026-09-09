d5fe95f0d5537d8c98a584083f6a7f917285bf8e
Radouane Khouadri <redouane.khouadri_externe@rte-france.com>
2026-09-09 15:16:38 +0200

    adress reviews

--- src/features/process-config/components/GeneralInformationSection.tsx
+++ src/features/process-config/components/GeneralInformationSection.tsx
@@ -5,17 +5,12 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import {
-    DescriptionField,
-    DirectoryItemInput,
-    ElementType,
-    FieldConstants,
-    UniqueNameInput,
-} from '@gridsuite/commons-ui';
+import { DescriptionField, ElementType, FieldConstants, UniqueNameInput } from '@gridsuite/commons-ui';
 import { Grid, Typography } from '@mui/material';
 import { UUID } from 'node:crypto';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { FormSection } from 'shared/ui/FormSection';
+import DirectoryItemInput from './DirectoryItemInput';
 
 type GeneralInformationSectionProps = {
     directoryId: string | undefined;
@@ -27,21 +22,20 @@ export function GeneralInformationSection({ directoryId }: GeneralInformationSec
     return (
         <FormSection id="general-information-heading" title={<FormattedMessage id="processConfigGeneralInformation" />}>
             <Grid container spacing={2}>
-                <UniqueNameInput
-                    name={FieldConstants.NAME}
-                    label="processConfigName"
-                    elementType={ElementType.PROCESS_CONFIG}
-                    activeDirectory={directoryId as UUID}
-                    autoFocus
-                    formProps={{ size: 'small' }}
-                />
-
                 <Grid size={{ xs: 12, sm: 12 }}>
+                    <UniqueNameInput
+                        name={FieldConstants.NAME}
+                        label="processConfigName"
+                        elementType={ElementType.PROCESS_CONFIG}
+                        activeDirectory={directoryId as UUID}
+                        autoFocus
+                        formProps={{ size: 'small' }}
+                    />
                     <DescriptionField buttonLabel="AddOptionalDescription" buttonSx={{ textTransform: 'none' }} />
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 12 }}>
-                    <Typography variant="body2" fontWeight={500} gutterBottom>
+                    <Typography variant="subtitle1">
                         <FormattedMessage id="processConfigSaveDirectory" />
                     </Typography>
                     <DirectoryItemInput
@@ -49,7 +43,6 @@ export function GeneralInformationSection({ directoryId }: GeneralInformationSec
                         types={[ElementType.DIRECTORY]}
                         multiSelect={false}
                         onlyLeaves={false}
-                        buttonSx={{ textTransform: 'none' }}
                         validationButtonText={intl.formatMessage({ id: 'validate' })}
                         title={intl.formatMessage({ id: 'processConfigSaveDirectory' })}
                     />
