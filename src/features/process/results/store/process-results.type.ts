1ab076aaca1b989f4e4847f71d71ed37f246a82a
Thang PHAM <phamthang37@gmail.com>
2026-09-15 18:19:05 +0200

    Custom Aggrid typing
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/features/process/results/store/process-results.type.ts
+++ src/features/process/results/store/process-results.type.ts
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-import { FilterConfig, TableSortConfig } from '@gridsuite/commons-ui';
+import { FilterConfig, TableSort } from '@gridsuite/commons-ui';
 import { UUID } from 'node:crypto';
 
 interface TablesState {
@@ -16,7 +16,7 @@ export type TableFiltersState = {
 };
 
 export interface ProcessResultsState {
-    tableSort: Record<string, TableSortConfig>;
+    tableSort: TableSort;
     tables: TablesState;
     tableFilters: TableFiltersState;
 }
