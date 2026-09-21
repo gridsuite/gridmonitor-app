bae861a235a45963587f54fbb8713eb35ea958af
BOUHOURS Antoine <antoine.bouhours@rte-france.com>
2026-09-21 15:58:28 +0200

    Fix merge

--- src/features/process/execute/components/ProcessTypeStep.tsx
+++ src/features/process/execute/components/ProcessTypeStep.tsx
@@ -13,7 +13,7 @@ import { useIntl } from 'react-intl';
 export function ProcessTypeStep({ control }: { control: any }) {
     const intl = useIntl();
     return (
-        <Stack spacing={0.5} paddingTop={1}>
+        <Stack spacing={0.5} sx={{ pt: 1 }}>
             <SelectInput
                 name="processType"
                 label="processType"
